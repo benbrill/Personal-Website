ff0b17fec67ec377e1ef1b9850fc734b20741e2f
Ben Brill <benbrill101@gmail.com>
2021-11-30 17:48:30 -0800

    Change Navbar

--- test_site/src/pages/resume.jsx
+++ test_site/src/pages/resume.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Container } from 'react-bootstrap'
 
 const resume = () => {
     return (
         <div>
             <Layout>
-                <h1>This page is in development</h1>
+                <Container fluid = "true" style = {{paddingTop: "20px", display: "flex"}}>
+                    <StaticImage src = "../images/Washington-DC-ProfileInsta.jpg" style={{borderRadius: "50%", marginRight: "40px"}} width = {200} aspectRatio = {1}/>
+                    <div style = {{flex: 1}}>
+                        <h1 style = {{fontWeight: 600, fontSize: "3.5rem"}}>Ben Brill</h1>
+                        <p>Data Science/Statistics</p>
+                        <div style = {{display: "flex", justifyContent: "space-between"}}>
+                            <div>Email</div>
+                            <div>github</div>
+                            <div>twitter</div>
+                            <div>LinkedIn</div>
+                        </div>
+                    </div>
+                </Container>
+                
             </Layout>
             
         </div>
