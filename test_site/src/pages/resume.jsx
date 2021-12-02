5a465992f43581f18c97bdcdc69fef51e9c6a3de
Ben Brill <benbrill101@gmail.com>
2021-12-02 15:35:20 -0800

    Test Resume update

--- test_site/src/pages/resume.jsx
+++ test_site/src/pages/resume.jsx
@@ -3,13 +3,21 @@ import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import { Container } from 'react-bootstrap'
 import { graphql } from "gatsby"
+import Seo from '../components/seo'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const resume = ({data}) => {
     return (
+
         <div>
+            <Seo title="Resume" />
             <Layout>
-                <Container fluid = "true" style = {{paddingTop: "20px", display: "flex"}}>
-                    <StaticImage src = "../images/Washington-DC-ProfileInsta.jpg" style={{borderRadius: "50%", marginRight: "40px"}} width = {200} aspectRatio = {1}/>
+              <Container fluid = "true">
+                <Row>
+                  <Col lg={4}>
+                <div style = {{paddingTop: "20px", display: "flex", flexDirection: "column"}}>
+                    <StaticImage src = "../images/Washington-DC-ProfileInsta.jpg" style={{borderRadius: "50%", marginRight: "40px"}} width = {250} aspectRatio = {1}/>
                     <div style = {{flex: 1}}>
                         <h1 style = {{fontWeight: 600, fontSize: "3.5rem"}}>Ben Brill</h1>
                         <p>Data Science/Statistics</p>
@@ -20,12 +28,22 @@ const resume = ({data}) => {
                             <div>LinkedIn</div>
                         </div>
                     </div>
-                {data.allDataYaml.nodes[0].Education.map(element => {
-                    <h1>{element.Name}</h1>
-                })}
+                </div>
+                </Col>
+                <Col lg = {4}>
+                <div style = {{paddingTop: "20px", display: "flex", flexDirection: "column"}}>
+                <h1>Education</h1>
+                {data.allDataYaml.nodes[0].Education.map(element => (
+                    <div style = {{paddingTop: "20px", display: "flex", flexDirection: "column"}}>
+                        <h2 style = {{fontWeight: 4}}>{element.Name}</h2>
+                        <p>{element.Details}</p>
+                    </div>
+                ))}
+                </div>
+                </Col>
+                </Row>
                 </Container>
             </Layout>
-            
         </div>
     )
 }
