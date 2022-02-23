b37438543081dcebea6369ef64838a9066531855
Ben Brill <benbrill101@gmail.com>
2022-02-22 23:08:37 -0800

    Update ticket prices

--- test_site/src/components/ticket_graphs/SevenGraph.jsx
+++ test_site/src/components/ticket_graphs/SevenGraph.jsx
@@ -34,14 +34,15 @@ const InitialFinalGraph = () => {
     var data = OrigData.filter(ticket => ticket.homeTeam === team)
     return (
         <div>
+            <h4>Percent change in price from 7 days prior to gametime</h4>
             <ToggleButtonGroup type = "radio" name = "teams" value = {team} onChange = {handleChange}>
             {teams.map(cteam => <ToggleButton key={cteam} onClick={() => setteam(cteam)} style={{backgroundColor: teamStyles[cteam].color, color: teamStyles[cteam].textColor, borderColor: "#000000"}}>{cteam}</ToggleButton>)}
             </ToggleButtonGroup>
             <ResponsiveContainer width="100%" height={350} margin={{left: 20, bottom: 20}}>
             <LineChart data={data}>
-                <Line type="monotone" dataKey="diff_7" stroke={teamStyles[team].color} />
+                <Line type="monotone" dataKey="diff_7" stroke={teamStyles[team].color} name = "% difference"/>
                 <XAxis dataKey="days_to_game" reversed={true} label = {{value: "Days to Game", dy: 15, fontFamily: 'halyard-display, sans-serif'}} type = "number" tickCount={8} domain={[0, 7]} height = {50} fontFamily = {'halyard-display, sans-serif'} />
-                <YAxis dataKey = "diff_7" label = {{value : "Difference from price 7 days before game", angle : -90, dx: -20, fontFamily: 'halyard-display, sans-serif'}} width = {60} tickCount = {3} type = "number" fontSize = {15} fontFamily = {'halyard-display, sans-serif'} ticks = {[-0.5, 0, 0.5]}/>
+                <YAxis dataKey = "diff_7" label = {{value : "Difference from price 7 days before game", angle : -90, dx: -20, fontFamily: 'halyard-display, sans-serif'}} width = {60} tickCount = {3} type = "number" fontSize = {15} fontFamily = {'halyard-display, sans-serif'} ticks = {[-50, 0, 50]}/>
                 {/* <ReferenceLine y="0" stroke="green" label="Min PAGE" /> */}
                 <CartesianGrid strokeDasharray="3 3"/>
                 <Tooltip label="Days to Game"/>
