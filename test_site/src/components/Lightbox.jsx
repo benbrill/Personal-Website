eb51a76db357b5e2725f625bf41b6120c41a69fd
Ben Brill <benbrill101@gmail.com>
2023-07-27 19:26:15 -0700

    Make modal bigger

--- test_site/src/components/Lightbox.jsx
+++ test_site/src/components/Lightbox.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import '../App.scss'; // import the CSS file
+import '../App.scss';
 
 const ImageLightbox = ({ imageData }) => {
     const [show, setShow] = useState(false);
@@ -17,10 +17,17 @@ const ImageLightbox = ({ imageData }) => {
                 <GatsbyImage image={image} alt="Thumbnail image" />
             </div>
 
-            <Modal show={show} onHide={handleClose} dialogClassName="custom-modal" centered size="lg">
+            <Modal
+                show={show}
+                onHide={handleClose}
+                dialogClassName="custom-modal"
+                centered
+                onEntered={() => { document.body.style.overflow = 'hidden'; }}
+                onExited={() => { document.body.style.overflow = 'auto'; }}
+            >
                 <span className="close-btn" onClick={handleClose}>&times;</span>
-                <Modal.Body>
-                    <GatsbyImage image={image} alt="Modal content" style={{ width: '100%' }} placeholder = "blurred"/>
+                <Modal.Body style={{ padding: 0 }}>
+                    <GatsbyImage image={image} alt="Modal content" style={{ width: 'auto', height: 'auto' }} />
                 </Modal.Body>
             </Modal>
         </>
