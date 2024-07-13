f69ee0d6266dfd9cad528d3fc28c889e3ba6e3da
your_username <benbrill101@gmail.com>
2024-07-12 20:43:01 -0700

    layout_home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import Layout from "../components/layout_home"
+import Layout from "../components/LayoutHome"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
