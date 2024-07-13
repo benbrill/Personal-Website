dcd35ae8835f6bcce4a63aa2660b8c6307376afa
your_username <benbrill101@gmail.com>
2024-07-12 19:48:55 -0700

    Remove responsive masonry grid

--- src/pages/photos/{photoBlogYaml.web_path}.jsx
+++ src/pages/photos/{photoBlogYaml.web_path}.jsx
@@ -16,15 +16,13 @@ const PhotoBlogPage = ({ data }) => {
                 <h1 style = {{textAlign:"center", fontWeight:600, paddingTop: "30px"}}>{post.photoBlogYaml.name}</h1>
                 <p style = {{textAlign: "center", marginBottom: "5px"}}>{post.photoBlogYaml.description}</p>
                 <p style = {{textAlign: "center"}}><em>{post.photoBlogYaml.date}</em></p>
-                <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}>
-                <Masonry gutter = "10px">
-                    {post.photoBlogYaml.images.map(image => (
+                <div style = {{padding: "0px 0em"}}>
+                {post.photoBlogYaml.images.map(image => (
 
-                            <Lightbox imageData = {image.childImageSharp.gatsbyImageData}/>
+                        <Lightbox imageData = {image.childImageSharp.gatsbyImageData}/>
 
-                    ))}
-                </Masonry>
-            </ResponsiveMasonry>
+                ))}
+                </div>
         </>
     )
 }
